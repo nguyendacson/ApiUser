a6d1ee37615bee8b2c20c621a9ea45b217f256da
nguyendacson <dacson1822003@gmail.com>
2025-07-22 17:57:25 +0700

    add code

--- src/main/java/com/example/ApiUser/configuration/SecurityConfig.java
+++ src/main/java/com/example/ApiUser/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +22,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Value("${jwt.signerKey}")
@@ -33,11 +35,9 @@ public class SecurityConfig {
                 request
                         .requestMatchers(HttpMethod.POST,PUBLIC_ENDPOINTS).permitAll()
 
-                        .requestMatchers(HttpMethod.GET, "/users")
-                        .hasRole(Role.ADMIN.name())
+//                        .requestMatchers(HttpMethod.GET, "/users")
+//                        .hasRole(Role.ADMIN.name())
 //                        .hasAnyAuthority("ROLE_ADMIN")
-
-
                 .anyRequest().authenticated());
 
         httpSecurity.oauth2ResourceServer(oauth2 ->
