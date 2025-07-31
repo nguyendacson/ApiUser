6de671ee36b14a17b1432b6a7c5b801ba3abee5d
nguyendacson <dacson1822003@gmail.com>
2025-07-31 23:57:02 +0700

    done Exception

---
+++ src/main/java/com/example/ApiUser/configuration/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,38 @@
+package com.example.ApiUser.configuration;
+
+import com.example.ApiUser.dto.response.ApiResponse;
+import com.example.ApiUser.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+//import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@EnableWebSecurity
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+        response.setContentType("application/json");
+
+        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        new ObjectMapper().writeValue(response.getOutputStream(), apiResponse);
+    }
+}
+
